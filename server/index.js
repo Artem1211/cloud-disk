6a732d10266a032843cd4d0eacac80a4c4caa195
Artem <N.Artem.work@yandex.ru>
2021-03-15 23:06:46 +0300

    форма регистрации

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express")
 const mongoose = require("mongoose")
 const config = require("config")
 const authRouter = require("./routes/auth.routes")
+const corsMiddleware = require("./middleware/cors.middleware")
 const app = express()
 const PORT = config.get('serverPort')
 
+app.use(corsMiddleware)
 app.use(express.json())
 console.log(config.get("dbUrl"));
 app.use("/api/auth", authRouter)
