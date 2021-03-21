77a4975737f2b34e1109af5f2c149304dc791bbc
Artem <N.Artem.work@yandex.ru>
2021-03-21 23:08:02 +0300

    Работа с папками, их добавление и получение списка

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.scss'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Registration from "./registration/Registration";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Registration from "./authorization/Registration";
+import Login from './authorization/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from '../actions/user';
+import Disk from './disk/Disk';
 
 function App() {
+
+    const isAuth = useSelector(state => state.user.isAuth)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(auth())
+    }, [])
     return (
         <BrowserRouter>
             <div className='app'>
                 <Navbar />
                 <div className="wrap">
-                    <Switch>
-                        <Route path="/registration" component={Registration} />
-                    </Switch>
+                    {
+                        !isAuth ?
+                            <Switch>
+                                <Route path="/registration" component={Registration} />
+                                <Route path="/login" component={Login} />
+                                <Redirect to="/login" />
+                            </Switch> :
+                            <Switch>
+                                <Route exact path="/" component={Disk} />
+                                <Redirect to="/" />
+                            </Switch>
+                    }
+
                 </div>
             </div>
         </BrowserRouter>
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,7 +5,9 @@ const config = require("config")
 const { check, validationResult } = require("express-validator")
 const jwt = require('jsonwebtoken')
 const router = new Router()
-
+const authMiddleware = require('../middleware/auth.middleware')
+const fileService = require('../services/fileService')
+const File = require('../models/File')
 
 router.post('/registration',
     [
@@ -30,6 +32,7 @@ router.post('/registration',
             const hashPassword = await bcrypt.hash(password, 8)
             const user = new User({ email, password: hashPassword })
             await user.save()
+            await fileService.createDir(new File({ user: user.id, name: '' }))
             return res.json({ message: "User was created" })
 
         } catch (e) {
@@ -58,7 +61,7 @@ router.post('/login',
                     id: user.id,
                     email: user.email,
                     diskSpace: user.diskSpace,
-                    userSpace: user.userSpace,
+                    usedSpace: user.usedSpace,
                     avatar: user.avatar
                 }
             })
@@ -67,6 +70,28 @@ router.post('/login',
             res.send({ message: "Server error" })
         }
     })
+
+router.get('/auth', authMiddleware,
+    async (req, res) => {
+        try {
+            const user = await User.findOne({ _id: req.user.id })
+            const token = jwt.sign({ id: user.id }, config.get("secretKey"), { expiresIn: "1h" })
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({ message: "Server error" })
+        }
+    })
+
 
 
 module.exports = router
