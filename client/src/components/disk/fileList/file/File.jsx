1f3a8bbda085e52211e12bbc78d84abfe5071aa6
Artem <N.Artem.work@yandex.ru>
2021-03-25 21:00:05 +0300

    Загрузка файлов

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './file.scss'
 import dirLogo from '../../../../assets/img/dir.svg'
 import fileLogo from '../../../../assets/img/file.svg'
+import { useDispatch, useSelector } from 'react-redux';
+import { pushToStack, seеCurrentDir } from '../../../../reducers/fileReducer';
 const File = ({ file }) => {
+    const dispatch = useDispatch()
+    const currentDir = useSelector(state => state.files.currentDir)
+
+    function openDirHandler(file) {
+        if (file.type === 'dir') {
+            dispatch(pushToStack(currentDir))
+            dispatch(seеCurrentDir(file._id))
+        }
+    }
     return (
-        <div className='file'>
+        <div className='file' onClick={() => openDirHandler(file)}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img" />
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0, 10)}</div>
